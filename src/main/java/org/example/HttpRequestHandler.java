4adef4dd7a965e070435289b34a953c1300c4f63
lgh02 <xzvxv3@gmail.com>
2026-01-28 20:23:41 +0900

    /site1, /site2, /search 추가

--- src/main/java/org/example/HttpRequestHandler.java
+++ src/main/java/org/example/HttpRequestHandler.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.URLDecoder;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -40,12 +41,76 @@ public class HttpRequestHandler implements Runnable {
             log.info("클라이언트의 HTTP 요청 정보 출력\n{}", clientRequest);
 
             log.info("HTTP 응답 생성중...");
-            Thread.sleep(3000); // 서버의 요청 처리 시간
 
-            // 클라이언트에게 응답 처리
-            responseToClient(writer);
+            Thread.sleep(1500); // 서버의 요청 처리 시간
+
+            if(clientRequest.startsWith("GET /site1")) {
+                site1(writer);
+            } else if(clientRequest.startsWith("GET /site2")) {
+                site2(writer);
+            } else if(clientRequest.startsWith("GET /search")) {
+                search(writer, clientRequest);
+            } else if(clientRequest.startsWith("GET / ")) {
+                home(writer);
+            } else {
+                notFound(writer);
+            }
+
             log.info("HTTP 응답 전달 완료");
         }
+    }
+
+    private static void home(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>home</h1>");
+        writer.println("<ul>");
+        writer.println("<li><a href='/site1'>site1</a></li>");
+        writer.println("<li><a href='/site2'>site2</a></li>");
+        writer.println("<li><a href='/search?q=hello'>검색</a></li>");
+        writer.println("</ul>");
+        writer.flush();
+    }
+
+    private static void site1(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>site1</h1>");
+        writer.flush();
+    }
+
+    private static void site2(PrintWriter writer) {
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>site2</h1>");
+        writer.flush();
+    }
+
+    private static void notFound(PrintWriter writer) {
+        writer.println("HTTP/1.1 404 Not Found");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>404 페이지를 찾을 수 없습니다.</h1>");
+        writer.flush();
+    }
+
+    private static void search(PrintWriter writer, String requestString) {
+        int startIndex = requestString.indexOf("q=");
+        int endIndex = requestString.indexOf(" ", startIndex+2);
+        String query = requestString.substring(startIndex+2, endIndex);
+        String decode = URLDecoder.decode(query, UTF_8);
+        writer.println("HTTP/1.1 200 OK");
+        writer.println("Content-Type: text/html; charset=UTF-8");
+        writer.println();
+        writer.println("<h1>Search</h1>");
+        writer.println("<ul>");
+        writer.println("<li>query: " + query + "</li>");
+        writer.println("<li>decode: " + decode + "</li>");
+        writer.println("</ul>");
+        writer.flush();
     }
 
     // 클라이언트의 요청을 문자열로 변환해서 반환
@@ -58,23 +123,5 @@ public class HttpRequestHandler implements Runnable {
             sb.append(line).append("\n");
         }
         return sb.toString();
-    }
-
-    // 클라이언트에게 응답 처리
-    private void responseToClient(PrintWriter writer) {
-        String body = "<h1>Hello World</h1>";
-        int length = body.getBytes(UTF_8).length;
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("HTTP/1.1 200 OK\r\n");
-        sb.append("Content-Type: text/html\r\n");
-        sb.append("Content-Length: ").append(length).append("\r\n");
-        sb.append("\r\n"); // header, body 구분 라인
-        sb.append(body);
-
-        log.info("HTTP 응답 정보 출력\n{}", sb);
-
-        writer.println(sb);
-        writer.flush();
     }
 }
