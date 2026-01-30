9960bdd5c84a2166f02e34aa8c5590108180dc44
lgh02 <xzvxv3@gmail.com>
2026-01-31 02:05:02 +0900

    애노테이션 서블릿 성능 최적화 O(n) -> O(1)

--- src/main/java/org/example/annotation/AnnotationServlet.java
+++ src/main/java/org/example/annotation/AnnotationServlet.java
@@ -3,56 +3,81 @@ package org.example.annotation;
 import lombok.RequiredArgsConstructor;
 import org.example.HttpRequest;
 import org.example.HttpResponse;
+import org.example.exception.PageNotFoundException;
 import org.example.servlet.HttpServlet;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@RequiredArgsConstructor
+
 public class AnnotationServlet implements HttpServlet {
 
-    private final List<Object> controllers;
+    private final Map<String, ControllerMethod> pathMap;
 
-    @Override
-    public void service(HttpRequest request, HttpResponse response) throws IOException {
-        String path = request.getPath();
+    public AnnotationServlet(List<Object> controllers) {
+        this.pathMap = new HashMap<>();
+        initializePathMap(controllers);
+    }
 
+    private void initializePathMap(List<Object> controllers) {
         for (Object controller : controllers) {
-            Method[] methods = controller.getClass().getDeclaredMethods();
-            for (Method method : methods) {
+            for(Method method : controller.getClass().getDeclaredMethods()) {
                 if(method.isAnnotationPresent(Mapping.class)) {
-                    Mapping mapping = method.getAnnotation(Mapping.class);
-                    String value = mapping.value();
-                    if(value.equals(path)) {
-                        invoke(controller, method, request, response);
+                    String path = method.getAnnotation(Mapping.class).value();
+
+                    if(pathMap.containsKey(path)) {
+                        ControllerMethod controllerMethod = pathMap.get(path);
+                        throw new IllegalArgumentException("경로 중복 등록, path=" + path + ", method=" + method + ", 이미 등록된 메서드=" + controllerMethod.method);
                     }
+
+                    pathMap.put(path, new ControllerMethod(controller, method));
                 }
             }
         }
     }
 
-    private static void invoke(Object controller, Method method, HttpRequest request, HttpResponse response) {
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        Object[] args = new Object[parameterTypes.length];
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        String path = request.getPath();
+        ControllerMethod controllerMethod = pathMap.get(path);
 
-        for(int i = 0; i < parameterTypes.length; i++) {
-            if(parameterTypes[i] == HttpRequest.class) {
-                args[i] = request;
-            }
-            else if(parameterTypes[i] == HttpResponse.class) {
-                args[i] = response;
-            }
-            else {
-                throw new IllegalArgumentException("Unsupported parameter type: " + parameterTypes[i]);
-            }
+        if (controllerMethod == null) {
+            throw new PageNotFoundException("request=" + path);
         }
 
-        try {
-            method.invoke(controller, args);
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException(e);
+        controllerMethod.invoke(request, response);
+    }
+
+    @RequiredArgsConstructor
+    private static class ControllerMethod {
+        private final Object controller;
+        private final Method method;
+
+        public void invoke(HttpRequest request, HttpResponse response) {
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            Object[] args = new Object[parameterTypes.length];
+
+            for(int i = 0; i < parameterTypes.length; i++) {
+                if(parameterTypes[i] == HttpRequest.class) {
+                    args[i] = request;
+                }
+                else if(parameterTypes[i] == HttpResponse.class) {
+                    args[i] = response;
+                }
+                else {
+                    throw new IllegalArgumentException("Unsupported parameter type: " + parameterTypes[i]);
+                }
+            }
+
+            try {
+                method.invoke(controller, args);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
